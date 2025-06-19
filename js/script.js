6d06a718e43f1a1ec4b50087857ae0e23c168c84
hardiksrivastavaa <hardikfgp@gmail.com>
2025-06-19 16:18:11 +0530

    Latest Updates  19.6.2025

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const downloadBtn = document.getElementById("download-btn");
+const generateBtn = document.getElementById("generate-btn");
 const branchDropdown = document.getElementById("branch");
 const sessionDropdown = document.getElementById("session");
 const yearSemDropdown = document.getElementById("yearSem");
@@ -7,13 +9,16 @@ const form = document.getElementById("frontPageForm");
 
 // Function to populate subjects dropdown based on selected branch and yearSem
 const populateSubjects = () => {
-  if (!(branchDropdown.value in subjectsData) || !(yearSemDropdown.value in subjectsData[branchDropdown.value])) {
+  if (
+    !(branchDropdown.value in subjectsData) ||
+    !(yearSemDropdown.value in subjectsData[branchDropdown.value])
+  ) {
     updateDropdown(subjectDropdown, []);
     return;
   }
   const subjects = subjectsData[branchDropdown.value][yearSemDropdown.value];
   updateDropdown(subjectDropdown, subjects);
-}
+};
 
 // Function to populate teacher dropdown based on selected branch
 const populateTeachers = () => {
@@ -23,7 +28,7 @@ const populateTeachers = () => {
   }
   const teachers = teachersData[branchDropdown.value];
   updateDropdown(teacherDropdown, teachers);
-}
+};
 
 // Function to handle dropdown change based on selected branch and yearSem
 const handleDropdownChange = () => {
@@ -33,12 +38,11 @@ const handleDropdownChange = () => {
 
 const showErrorModal = () => {
   document.getElementById("errorModal").classList.remove("hidden");
-}
+};
 
 const closeErrorModal = () => {
   document.getElementById("errorModal").classList.add("hidden");
-}
-
+};
 
 // Event listener to submit form and generate front page PDF
 form.addEventListener("submit", async (e) => {
@@ -47,7 +51,7 @@ form.addEventListener("submit", async (e) => {
     branch: branchDropdown.value,
     yearSem: yearSemDropdown.value,
     subjects: subjectDropdown.value,
-    nameInput: document.getElementById("nameInput").value.trim()
+    nameInput: document.getElementById("nameInput").value.trim(),
   };
 
   try {
@@ -67,19 +71,23 @@ form.addEventListener("submit", async (e) => {
       body: JSON.stringify(formData),
     }
   ).catch((error) => console.error("Error:", error));
-
 });
 
 // Function to generate front page PDF
 const generateFrontPage = async () => {
-
   const branch = branchDropdown.options[branchDropdown.selectedIndex].innerText;
-  const session = sessionDropdown.options[sessionDropdown.selectedIndex].innerText;
-  const yearSem = yearSemDropdown.options[yearSemDropdown.selectedIndex].innerText;
-  const teacher = teacherDropdown.options[teacherDropdown.selectedIndex].innerText;
-  const subject = subjectDropdown.options[subjectDropdown.selectedIndex].innerText;
+  const session =
+    sessionDropdown.options[sessionDropdown.selectedIndex].innerText;
+  const yearSem =
+    yearSemDropdown.options[yearSemDropdown.selectedIndex].innerText;
+  const teacher =
+    teacherDropdown.options[teacherDropdown.selectedIndex].innerText;
+  const subject =
+    subjectDropdown.options[subjectDropdown.selectedIndex].innerText;
   const studentName = document.getElementById("nameInput").value.trim();
-  const studentEnrollment = document.getElementById("enrollmentInput").value.trim();
+  const studentEnrollment = document
+    .getElementById("enrollmentInput")
+    .value.trim();
 
   const yPositions = {
     branchY: 265,
@@ -95,40 +103,106 @@ const generateFrontPage = async () => {
   const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
   const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
   const page = pdfDoc.getPage(0);
-  const timesRomanFont = await pdfDoc.embedFont(PDFLib.StandardFonts.TimesRoman);
-  const timesRomanBoldFont = await pdfDoc.embedFont(PDFLib.StandardFonts.TimesRomanBold);
+  const timesRomanFont = await pdfDoc.embedFont(
+    PDFLib.StandardFonts.TimesRoman
+  );
+  const timesRomanBoldFont = await pdfDoc.embedFont(
+    PDFLib.StandardFonts.TimesRomanBold
+  );
 
   const pageWidth = page.getWidth();
 
   // Adjust name and enrollment positions based on page width
-  const nameX = pageWidth - timesRomanFont.widthOfTextAtSize(studentName, 23) - 20;
-  const enrollmentX = pageWidth - timesRomanFont.widthOfTextAtSize(studentEnrollment, 21) - 20;
+  const nameX =
+    pageWidth - timesRomanFont.widthOfTextAtSize(studentName, 23) - 20;
+  const enrollmentX =
+    pageWidth - timesRomanFont.widthOfTextAtSize(studentEnrollment, 21) - 20;
 
   // Adjust font sizes
   let branchFontSize = adjustFontSize(branch, timesRomanFont, pageWidth);
   let subjectFontSize = adjustFontSize(subject, timesRomanBoldFont, pageWidth);
 
   // Branch, Year & Sem, Session, Subject (centered)
-  await drawText(page, `Session - ${session}`, getCenteredX(`Session - ${session}`, 26, timesRomanFont, pageWidth), yPositions.sessionY, 26, [0, 0, 0], timesRomanFont);
-  await drawText(page, subject, getCenteredX(subject, subjectFontSize, timesRomanBoldFont, pageWidth), yPositions.subjectY, subjectFontSize, [61, 104, 180], timesRomanBoldFont);
-  await drawText(page, branch, getCenteredX(branch, branchFontSize, timesRomanFont, pageWidth), yPositions.branchY, branchFontSize, [255, 0, 0], timesRomanFont);
-  await drawText(page, `${yearSem}`, getCenteredX(yearSem, 24, timesRomanFont, pageWidth), yPositions.yearSemY, 24, [255, 0, 0], timesRomanFont);
+  await drawText(
+    page,
+    `Session - ${session}`,
+    getCenteredX(`Session - ${session}`, 26, timesRomanFont, pageWidth),
+    yPositions.sessionY,
+    26,
+    [0, 0, 0],
+    timesRomanFont
+  );
+  await drawText(
+    page,
+    subject,
+    getCenteredX(subject, subjectFontSize, timesRomanBoldFont, pageWidth),
+    yPositions.subjectY,
+    subjectFontSize,
+    [61, 104, 180],
+    timesRomanBoldFont
+  );
+  await drawText(
+    page,
+    branch,
+    getCenteredX(branch, branchFontSize, timesRomanFont, pageWidth),
+    yPositions.branchY,
+    branchFontSize,
+    [255, 0, 0],
+    timesRomanFont
+  );
+  await drawText(
+    page,
+    `${yearSem}`,
+    getCenteredX(yearSem, 24, timesRomanFont, pageWidth),
+    yPositions.yearSemY,
+    24,
+    [255, 0, 0],
+    timesRomanFont
+  );
 
   // Teacher (left aligned)
-  await drawText(page, `${teacher}`, 20, yPositions.teacherY, 23, [0, 0, 0], timesRomanFont);
+  await drawText(
+    page,
+    `${teacher}`,
+    20,
+    yPositions.teacherY,
+    23,
+    [0, 0, 0],
+    timesRomanFont
+  );
 
   // Student Name & Enrollment (right aligned)
-  await drawText(page, `${studentEnrollment}`, enrollmentX, yPositions.enrollmentY, 21, [0, 0, 0], timesRomanFont);
-  await drawText(page, `${studentName}`, nameX, yPositions.nameY, 23, [0, 0, 0], timesRomanFont);
+  await drawText(
+    page,
+    `${studentEnrollment}`,
+    enrollmentX,
+    yPositions.enrollmentY,
+    21,
+    [0, 0, 0],
+    timesRomanFont
+  );
+  await drawText(
+    page,
+    `${studentName}`,
+    nameX,
+    yPositions.nameY,
+    23,
+    [0, 0, 0],
+    timesRomanFont
+  );
 
   const pdfBytes = await pdfDoc.save();
   const blob = new Blob([pdfBytes], { type: "application/pdf" });
   const urlBlob = URL.createObjectURL(blob);
-  const link = document.getElementById("download-pdf");
-  link.href = urlBlob;
-  link.download = `${subject} - ${studentName}.pdf`;
-  link.click();
-}
+  downloadBtn.classList.remove("hidden");
+  generateBtn.classList.add("hidden");
+  downloadBtn.href = urlBlob;
+  downloadBtn.download = `${subject} - ${studentName}.pdf`;
+  //  downloadBtn.click();
+  // window.open(urlBlob);
+};
 
-
-
+form.addEventListener("change", () => {
+  downloadBtn.classList.add("hidden");
+  generateBtn.classList.remove("hidden");
+});
